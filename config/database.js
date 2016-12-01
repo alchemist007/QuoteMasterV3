cd864f65fe071f256e36ec2bd9683ce476e97b53
Nesh Basnet <hsenesh@gmail.com>
2016-11-30 21:51:33 -0500

    not working

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
-module.exports = {
-	url : 'mongodb://nesh:nesh@jello.modulusmongo.net:27017/iv4Erari/testdb' 
-};
+	module.exports = {
+		url : 'mongodb://nesh:nesh@jello.modulusmongo.net:27017/iv4Erari/testdb' 
+	};	
